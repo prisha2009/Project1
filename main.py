65c17b1a6ac0e62743d2c2d1d1e3ae1e4d66039a
Prisha <prisha_bansal@outlook.com>
2022-07-01 19:11:14 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy
+import matplotlib.pyplot as mt
+
+
+list1 = numpy.array([[1,2,3,4,5],[46,7,8,9,10]])
+print(list1)
+
+list2 = numpy.array([1,2])
+list3 = numpy.array([3,4])
+
+mt.plot(list2,list3)
+mt.show()
